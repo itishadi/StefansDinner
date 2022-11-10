db4fa34ca05916e1c4e483ccca675a923faa0c41
Hadi Rasouli <hadirasouli786@hotmail.com>
2022-11-10 13:52:56 +0100

    Add project files.

---
+++ StefansDinner/App.cs
@@ -0,0 +1,17 @@
+﻿using StefansDinner;
+
+public class App
+{
+    public void Run()
+    {
+        var round = 1;
+        var game = new Game();
+        while (true)
+        {
+            Console.WriteLine($"Spelomgång {round}");
+            game.Run();
+            System.Threading.Thread.Sleep(5000);
+            round++;
+        }
+    }
+}
